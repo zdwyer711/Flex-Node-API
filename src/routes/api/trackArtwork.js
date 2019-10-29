877f406cea231d2a5325863bb96f5dd4cc65bb76
Zach Dwyer <55959541+zdwyer711@users.noreply.github.com>
2019-10-28 23:13:23 -0500

    Fix Track Artwork GET Endpoint

--- src/routes/api/trackArtwork.js
+++ src/routes/api/trackArtwork.js
@@ -4,43 +4,71 @@
   const multer = require('multer');
   const fs = require('fs');
 
-  server.route( {
-        method: "POST",
-        path: "/api/trackArtwork/",
-         config: {
-              payload: {
-                      output: 'stream',
-                      allow: 'multipart/form-data' // important
-              },
-        },
-        handler:  {
-                  file: function(request) {
-                              try {
-                              console.log("handler request reached!")
-                              // const { payload } = request
-                              // console.log("Payload reached!")
-                              const data = request.payload;
-                              const file = data['trackArtwork'];
-                              //const file = request.files;
-                              //const response = handleTrackArtworkUpload(payload.file)
-                              console.log("Response reached!")
-                              //return res.recordset[0];
-                              return "Track Artowork Success!";
-                              } catch ( err ) {
-                                  server.log( [ "error", "api", "track" ], err );
-                                  return console.log("ERROR: " + err);
-                                }
-                    }
-                  }
-    } );
+  // server.route( {
+  //       method: "POST",
+  //       path: "/api/trackArtwork/",
+  //        config: {
+  //             payload: {
+  //                     //output: 'stream',
+  //                     allow: 'multipart/form-data' // important
+  //             },
+  //       },
+  //       handler:  {
+  //                 file: function(reques, h) {
+  //                             try {
+  //                             console.log("handler request reached!")
+  //                             // const { payload } = request
+  //                             // console.log("Payload reached!")
+  //                             const data = h.file(request.payload);
+  //                             const file = data['trackArtwork'];
+  //                             //const file = request.files;
+  //                             //const response = handleTrackArtworkUpload(payload.file)
+  //                             console.log("Response reached!")
+  //                             //return res.recordset[0];
+  //                             return "Track Artowork Success!";
+  //                             } catch ( err ) {
+  //                                 server.log( [ "error", "api", "track" ], err );
+  //                                 return console.log("ERROR: " + err);
+  //                               }
+  //                   }
+  //                 }
+  //   } );
+
+  server.route({
+  path: '/upload',
+  method: 'POST',
+  options: {
+    payload: {
+      output: 'stream',
+       'Content-Type': undefined
+    }
+  },
+  handler: async (req, h) => {
+    console.log("handler in track artwork reached.")
+    const { payload } = req
+    const response = handleFileUpload(payload.file)
+    return response
+  }
+})
 
     server.route({
         method: 'GET',
         path: '/youngSoul.jpg',
         handler: function (request, h) {
-          return h.file('/trackArtwork/youngSoul.jpg');
+          return h.file('./trackArtwork/youngSoul.jpg');
         }
     });
+
+    const handleFileUpload = file => {
+ return new Promise((resolve, reject) => {
+   fs.writeFile('./trackArtwork/test.png', file, err => {
+      if (err) {
+       reject(err)
+      }
+      resolve({ message: 'Upload successfully!' })
+   })
+ })
+}
 
     const handleTrackArtworkUpload = file => {
           return new Promise((resolve, reject) => {
